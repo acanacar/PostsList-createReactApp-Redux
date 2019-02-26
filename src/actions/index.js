cb8aaa54fdc8e84409b8c9685174248de537cddb
acanacar <ahmetcanacar.1@gmail.com>
2019-02-26 14:53:38 +0300

    redux-thunk is added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = async () => {
-
-  const response = await jsonPlaceholder.get('/posts')
-  return {
+export const fetchPosts = () => async dispatch => {
+  const response = jsonPlaceholder.get('/posts')
+  dispatch({
     type: 'FETCH_POSTS',
-    payload:response
-  }
+    payload: response,
+  })
 }
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
 
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-  <App />
+  <Provider store={store}>
+    <App />
   </Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
